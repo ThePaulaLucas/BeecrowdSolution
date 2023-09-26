4899501c73fbb6233d2953f00c0df1b7995caa67
Lucas de Paula <lucas-de-paula@hotmail.com>
2023-09-26 13:55:33 -0300

    Create 1001.cpp

---
+++ beginner/1001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+int main() {
+    using namespace std;
+    
+    int A, B, X;
+    cin >> A >> B;
+    
+    X = A + B;
+    
+    cout << "X = " << X << '\n';
+ 
+    return 0;
+}
