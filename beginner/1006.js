6530c456641efe8776bd0dcc14a3c35be8404608
Lucas de Paula <lucas-de-paula@hotmail.com>
2023-09-28 00:23:08 -0300

    Create 1006.js

---
+++ beginner/1006.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var a = parseFloat(lines.shift());
+var b = parseFloat(lines.shift());
+var c = parseFloat(lines.shift());
+
+var media = ((2 * a) + (3 * b) + (5 * c)) / 10;
+
+console.log('MEDIA = ' + media.toFixed(1));
