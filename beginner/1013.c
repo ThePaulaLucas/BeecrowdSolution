39be420d4af7d7e7389e66c9e70e179969b46c7e
Lucas de Paula <lucas-de-paula@hotmail.com>
2023-10-04 17:35:16 -0300

    Create 1013.c

---
+++ beginner/1013.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y, z, maior;
+	scanf("%d %d %d", &x, &y, &z);
+	if 	((x > y) && (x > z))
+		maior = x;
+	else if ((y > x) && (y > z))
+		maior = y;
+	else if ((z > x) && (z > y))
+		maior = z;
+	printf("%d eh o maior\n", maior);
+	return 0;
+}
